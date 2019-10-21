e079323adadee6cddd58bedf2c8aed80c59fe49d
Stefan Lemvig Frederiksen <stefanlemvigfrederiksen@gmail.com>
2019-10-21 16:15:50 +0200

    Moved AST types to separate file and fixed the convertStatements on while

--- Program.fs
+++ Program.fs
@@ -1,70 +1,6 @@
 ﻿// Learn more about F# at http://fsharp.org
 open System
-
-
-type Program = Declaration * Statement
-    
-and Statement =
-    | AssignmentL of L * A
-    | AssignmentR of String * A * A
-    | Statements of Statement * Statement
-    | IfStatement of B * Statement // Todo: Need an If-Else statement
-    | WhileStatement of B * Statement
-    | Read of L
-    | Write of A
-
-and Expression =
-    | ExpL of L
-    | ExpA of A
-    | ExpB of B
-
-and L =
-    | LabelX of String
-    | LabelA of String * A
-    | LabelFstR of String
-    | LabelSndR of String
-and A =
-    | ArithmeticN of int
-    | ArithmeticX of string
-    | ArithmeticA of string * int
-    | ArithmeticFstR of string
-    | ArithmeticSndR of string
-    | ROp of A * ArithmeticOperator * A
-and B =
-    | True
-    | False
-    | AOp of A * RelationalOperator * A
-    | BOp of B * BooleanOperator * B
-    | Not of B
-
-and Operator =
-    | OpA of ArithmeticOperator
-    | OpR of RelationalOperator
-    | OpB of BooleanOperator
-
-and ArithmeticOperator =
-        | Plus
-        | Minus
-        | Multiply
-        | Divide
-and RelationalOperator =
-        | LessThan
-        | GreaterThan
-        | LesserOrEqualTo
-        | GreaterOrEqualTo
-        | EqualTo
-        | NotEqualTo
-and BooleanOperator =
-        | AndOp
-        | OrOp
-
-
-and Declaration =
-        | DeclarationX of string                    // Declaration of variable 
-        | DeclarationA of string * int              // declaration of array 
-        | DeclarationR of string                    // Declaration of record
-        | DeclarationD of Declaration * Declaration
-
+open AbstractSyntaxTree
 
 
 // int[5] A;
@@ -94,7 +30,7 @@ and Node = int
 
 // q-1 (end) q0 (start) -> q1 -> q2 -> q3
 
-let convertToProgramGraph qstart qend (p: Program) =
+let convertToProgramGraph (p: Program) =
     let rec convertDeclaration ((qs, d): (int * Declaration)) =
         match d with
         | DeclarationX(x) -> ([Edge(qs, "int " + x + ";", qs+1)], qs+1) 
@@ -109,12 +45,15 @@ let convertToProgramGraph qstart qend (p: Program) =
     let rec convertStatements ((qs, s): (int * Statement)) =
         match s with
         | AssignmentL(l, a) -> ([Edge(qs, convertL l + ":=" + convertA a, qs + 1)], qs + 1) 
-        | AssignmentR(n, a1, a2) -> ([Edge(qs, "", qs + 1)], qs + 1) // Todo
+        | AssignmentR(n, a1, a2) -> ([Edge(qs, n + ":=(" + convertA a1 + "," + convertA a2, qs + 1)], qs + 1)
         | IfStatement(b, s) -> match convertStatements (qs+1, s) with
-                                | (edges, n) -> (edges@ [Edge(qs, "!" + convertB b, n+1); Edge(qs, convertB b, qs + 1)], n+1) // Todo
-        | WhileStatement (b, s) -> ([], qs + 1) // Todo
-        | Read(l) -> ([], qs + 1) // Todo
-        | Write (a) -> ([], qs + 1) // Todo
+                                | (edges, n) -> (edges@ [Edge(qs, "!" + convertB b, n); Edge(qs, convertB b, qs + 1)], n+1)
+        //| IfElseStatement(b, s1, s2) -> match convertStatements (qs+1, s1) with
+        //    | (edges1, n) -> match convertStatements (n+1, s2)
+        | WhileStatement (b, s) -> match convertStatements (qs+1, s) with
+                                    | (edges, n) -> (edges @ [Edge(qs, "!" + convertB b, n); Edge(qs, convertB b, qs + 1)], n+1)
+        | Read(l) -> ([Edge(qs, "read " + convertL l, qs+1)], qs + 1)
+        | Write (a) -> ([Edge(qs, "write" + convertA a, qs+1)], qs + 1)
         | Statements (s1, s2) -> 
             match convertStatements (qs, s1) with
               | (lst, cnt) -> match convertStatements (cnt, s2) with
@@ -260,12 +199,11 @@ let main argv =
                                                         AOp(ArithmeticA("A", 3), EqualTo, ArithmeticN(12)), // if (A[3] == 12) { alternative -> BOp(ArithmeticA("A", ArithmeticN(3)), EqualTo(), N(12))
                                                         AssignmentL(LabelX("x"), ArithmeticA("A", 4)) // x := A[x]; // is this by value or reference? (should be documented).
                                             )
-        
                                 )
                     )
     )
     
-    let graph = convertToProgramGraph 0 -1 ast
+    let graph = convertToProgramGraph ast
 
     printfn "set breakpoint to see ast value"
 
