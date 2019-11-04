d4874a3549fb48d9b1c5b4007d1a5a03d3d53324
Stefan Lemvig Frederiksen <stefanlemvigfrederiksen@gmail.com>
2019-11-04 08:47:59 +0100

    Fixed program graph convertions on if-else branching, by modifying the if's branch last nodes to point to the node generated after the else branch

--- Program.fs
+++ Program.fs
@@ -44,6 +44,11 @@ let convertToProgramGraph (p: Program) =
             | (lst, cnt) -> match convertDeclaration (cnt, d2) with
                             | (lst2, cnt2) -> (lst @ lst2, cnt2)
 
+    let fixIfElse (list: Edge List) (oldend: int) (newend: int) = 
+        list |> List.map (fun e -> 
+        match e with
+            | (qs, l, qe) when qe = oldend -> Edge(qs, l, newend)
+            | (qs, l, qe) -> Edge(qs, l, qe))
 
     let rec convertStatements ((qs, s): (int * Statement)) =
         match s with
@@ -51,8 +56,9 @@ let convertToProgramGraph (p: Program) =
         | AssignmentR(n, a1, a2) -> ([Edge(qs, n + ":=(" + convertA a1 + "," + convertA a2, qs + 1)], qs + 1)
         | IfStatement(b, s) -> match convertStatements (qs+1, s) with
                                 | (edges, n) -> (edges@ [Edge(qs, "!" + convertB b, n); Edge(qs, convertB b, qs + 1)], n+1)
-        //| IfElseStatement(b, s1, s2) -> match convertStatements (qs+1, s1) with
-        //    | (edges1, n) -> match convertStatements (n+1, s2)
+        | IfElseStatement(b, s1, s2) -> match convertStatements (qs+1, s1) with
+            | (edges1, n1) -> match convertStatements (n1, s2) with
+                | (edges2, n2) -> (fixIfElse edges1 n1 n2 @ edges2 @ [Edge(qs, convertB b, n1-1); Edge(qs, "!" + convertB b, n1)], n2)
         | WhileStatement (b, s) -> match convertStatements (qs+1, s) with
                                     | (edges, n) -> (edges @ [Edge(qs, "!" + convertB b, n); Edge(qs, convertB b, qs + 1)], n+1)
         | Read(l) -> ([Edge(qs, "read " + convertL l, qs+1)], qs + 1)
@@ -189,41 +195,47 @@ let convertToProgramGraph (p: Program) =
 [<EntryPoint>]
 let main argv =
     
-//    let ast = Program(
-//                    DeclarationD(
-//                                    DeclarationA("A", 5), // int[5] A;
-//                                    DeclarationX("x") // int x;
-//                    ), 
-//                    Statements(
-//                                AssignmentL(LabelX("x"), ArithmeticN(4)), // x := 4;
-//                                Statements(
-//                                            AssignmentL(LabelA("A", ArithmeticX("x")), ArithmeticN(2)), // A[x] := 2;  alternative -> AssignmentL(LabelA("A", ArithmeticX(LabelX("x"))), ArithmeticN(2))
-//                                            IfStatement(
-//                                                        AOp(ArithmeticA("A", 3), EqualTo, ArithmeticN(12)), // if (A[3] == 12) { alternative -> BOp(ArithmeticA("A", ArithmeticN(3)), EqualTo(), N(12))
-//                                                        AssignmentL(LabelX("x"), ArithmeticA("A", 4)) // x := A[x]; // is this by value or reference? (should be documented).
-//                                            )
-//                                )
-//                    )
-//    )
+    let ast = Program(
+                    DeclarationD(
+                                    DeclarationA("A", 5), // int[5] A;
+                                    DeclarationX("x") // int x;
+                    ), 
+                    Statements(
+                                
+                                IfElseStatement(
+                                    BoolValue(true),
+                                    AssignmentL(LabelX("a"), ArithmeticN(12)),
+                                    AssignmentL(LabelX("b"), ArithmeticN(5))
+                                ),
+                                AssignmentL(LabelX("x"), ArithmeticN(4)) // x := 4;
+                                //Statements(
+                                //            AssignmentL(LabelA("A", ArithmeticX("x")), ArithmeticN(2)), // A[x] := 2;  alternative -> AssignmentL(LabelA("A", ArithmeticX(LabelX("x"))), ArithmeticN(2))
+                                //            IfStatement(
+                                //                        AOp(ArithmeticA("A", 3), EqualTo, ArithmeticN(12)), // if (A[3] == 12) { alternative -> BOp(ArithmeticA("A", ArithmeticN(3)), EqualTo(), N(12))
+                                //                        AssignmentL(LabelX("x"), ArithmeticA("A", 4)) // x := A[x]; // is this by value or reference? (should be documented).
+                                //            )
+                                //)
+                    )
+    )
     
 
     
-    let x ="
-    {
-        int[5] a;
-        int x;
+    //let x ="
+    //{
+    //    int[5] a;
+    //    int x;
         
-        x := 4;
-        a[x] := 2;
-        if (a[3] == 12) {
-            x := a[x];
-        }
-    }"
-    // let graph = convertToProgramGraph ast
+    //    x := 4;
+    //    a[x] := 2;
+    //    if (a[3] == 12) {
+    //        x := a[x];
+    //    }
+    //}"
+     let graph = convertToProgramGraph ast
 
-    let tokens = parseString x
+    //let tokens = parseString x
     //let y = SqlParser.start SqlLexer.tokenize lexbuf   
-    printfn "%A" tokens  
+    //printfn "%A" tokens  
 
     printfn "set breakpoint to see ast value"
 
