d199c630f1582085f01d4b5dfa968135d6ae8702
Stefan Lemvig Frederiksen <stefanlemvigfrederiksen@gmail.com>
2019-11-17 16:00:52 +0100

    Added a small implementation of a Queue worklist for later use

---
+++ Worklist/Interface.fs
@@ -0,0 +1,10 @@
+﻿module Worklist.Interface
+
+type IWorklist<'T> = 
+  abstract member Empty : IWorklist<'T>
+
+  abstract member IsEmpty : bool
+
+  abstract member Insert : 'T -> IWorklist<'T>
+
+  abstract member Extract : 'T * IWorklist<'T>
---
+++ Worklist/Worklist.fs
@@ -0,0 +1,26 @@
+﻿module Worklist
+
+open Worklist.Interface
+open System.Collections.Generic
+
+type WorklistFifo<'T> (elems : seq<'T>) = 
+  member internal this.queue = 
+    let q = new Queue<'T>()
+    for e in elems do
+      q.Enqueue e
+    q
+
+  interface IWorklist<'T> with
+    member this.Empty = 
+      this.queue.Clear()
+      this :> IWorklist<'T>
+
+    member this.IsEmpty = this.queue.Count <= 0
+
+    member this.Insert e = 
+      this.queue.Enqueue e 
+      this :> IWorklist<'T>
+
+    member this.Extract = 
+      let head = this.queue.Dequeue()
+      (head, this :> IWorklist<'T>)
