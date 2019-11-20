229aac0c61b64f64a7faa1893f6b91b8f87bd280
Mathias Petersen <mathias.petersen95@gmail.com>
2019-11-20 19:53:57 +0100

    impl detection of signs; missing arrays

--- Analysis/DetectionOfSigns.fs
+++ Analysis/DetectionOfSigns.fs
@@ -5,18 +5,14 @@ open MonotoneFramework
 open AbstractSyntaxTree
 
 type Sign = Negative | Zero | Positive
-and DetectionOfSignsVar = string * Sign Set
-and DetectionOfSignsArr = string * Sign Set
-and DetectionOfSigns =  DetectionOfSignsVar Set * DetectionOfSignsArr Set
+and DetectionOfSigns =  Sign
 
 
 // {x, {-,0}}
 // {x, {+}}
 
-let initial (vars: string Set) (arrs: string Set) : DetectionOfSigns Set =
-  let dsVar = vars |> Set.map (fun x -> DetectionOfSignsVar(x, Set.ofList [Negative; Zero; Positive]))
-  let dsArr = arrs |> Set.map (fun x -> DetectionOfSignsArr(x, Set.ofList [Negative; Zero; Positive]))
-  (dsVar, dsArr)
+let initial (xs: string Set) : DetectionOfSigns AnalysisMapping =
+  xs |> Set.toSeq |> Seq.map(fun x -> (x, Set.ofList[Negative; Zero; Positive])) |> Map.ofSeq
 
 let signOf (n: int): Sign =
   if n < 0 then
@@ -92,7 +88,7 @@ let powerset (s1: Sign Set) (s2: Sign Set) (op: ArithmeticOperator) : Sign Set =
 
 let rec mapArith (arith: A) (ds: DetectionOfSigns AnalysisAssignment) (qs: Node) =
   let getResult (x: string) (ds: DetectionOfSigns AnalysisAssignment) =
-    ds.Item qs |> Set.toList |> Map.ofList |> Map.find x
+    ds.Item qs |> Map.find x
 
   match arith with
   | ArithmeticN(n) -> Set.singleton (signOf n)
@@ -104,40 +100,49 @@ let rec mapArith (arith: A) (ds: DetectionOfSigns AnalysisAssignment) (qs: Node)
                           | _ -> getResult x ds
   | ROp(a1, op, a2) -> powerset (mapArith a1 ds qs) (mapArith a2 ds qs) op
 
-let mapping (edge: Edge) (ds: DetectionOfSigns AnalysisAssignment): DetectionOfSigns Set =
+let mapping (edge: Edge) (ds: DetectionOfSigns AnalysisAssignment): DetectionOfSigns AnalysisMapping =
   let (qs, action, qe) = edge
   match action with
   | ActionAssignmentL(l, a) -> 
     match l with
-    | LabelX(x)
-    | LabelA(x, _)
-    | LabelFstR(x)
-    | LabelSndR(x) -> 
-  | ActionDeclarationX(x)
-  | ActionDeclarationA(x, _)
-  | ActionDeclarationR(x) -> Set.singleton(DetectionOfSigns(x, Set.singleton(Zero)))
-  | ActionAssignmentR(x, _, _) -> 
+    | LabelA(x, a1) -> failwith("figure out how to handle array index?") // Map.ofList [(x, mapArith a1 ds qs)]
+    | LabelX(x) -> Map.ofList [(x, mapArith a ds qs)]
+    | LabelFstR(x) -> Map.ofList [(x + ".fst", mapArith a ds qs)]
+    | LabelSndR(x) -> Map.ofList [(x + ".snd", mapArith a ds qs)]
+  | ActionDeclarationR(x) -> Map.ofList [(x + ".fst", Set.ofList [Zero]) ; (x + ".snd", Set.ofList [Zero])]
+  | ActionDeclarationX(x) -> Map.ofList [(x, Set.ofList [Zero])]
+  | ActionDeclarationA(x, _) -> failwith("impl")
+  | ActionAssignmentR(x, a1, a2) -> Map.ofList [(x + ".fst", mapArith a1 ds qs) ; (x + ".snd", mapArith a2 ds qs)]
   | ActionRead(l) -> 
     match l with
-    | LabelX(x)
-    | LabelA(x, _) -> 
-    | _ -> Set.empty
-  | _ -> Set.empty
+    | LabelX(x) -> failwith("impl")
+    | LabelA(x, a1) -> failwith("impl")
+    | _ -> Map.empty
+  | _ -> Map.empty
 
+let relation (t1: DetectionOfSigns AnalysisMapping) (t2: DetectionOfSigns AnalysisMapping) : bool =
+  Map.forall (fun key value -> Set.isSubset value (t2.Item key) ) t1
+
+let join (t1: DetectionOfSigns AnalysisMapping) (t2: DetectionOfSigns AnalysisMapping) : DetectionOfSigns AnalysisMapping =
+  // Good luck
+  Map.fold (fun acc key value -> acc.Add(key, (acc.Item key + value))) t1 t2
+
+let bottom (xs: string Set) : DetectionOfSigns AnalysisMapping = 
+  xs |> Set.toSeq |> Seq.map (fun x -> (x, Set.empty)) |> Map.ofSeq
 
 let analyse (pg: ProgramGraph) = 
   let (qs, _, _) = pg
   let domain : DetectionOfSigns AnalysisDomain = 
     {
-      relation = Set.isSubset
-      join = Set.union
-      bottom = Set.empty
+      relation = relation
+      join = join
+      bottom = bottom (ProgramGraph.variables pg)
     }
 
   let spec : DetectionOfSigns AnalysisSpecification = 
     {
       domain = domain
-      mapping = 
+      mapping = mapping
       initial = initial (ProgramGraph.variables pg)
     }
 
